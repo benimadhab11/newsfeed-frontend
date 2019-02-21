19ec9d0dcb49a92b9413ab45b40ca29cc3c469e0
Beni Madhab Kayal <beni.madhab.kayal@sap.com>
2019-02-21 13:58:16 +0530

    authentication

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import LoginForm from '../forms/LoginForm';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { login } from '../../actions/auth';
 
-const LoginPage = () => ( 
-  <div>
-  	<h1>LoginPage</h1>
-  </div>
-);
+export class LoginPage extends React.Component {
+	submit = data => this.props.login(data).then(() => this.props.history.push("/"));
 
-export default LoginPage;
+	render() {
+		return (
+			<div>
+			  	<h1>LoginPage</h1>
+			  	<LoginForm submit={this.submit} />
+		  </div>
+		);
+	}
+}
+
+LoginPage.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired
+  }).isRequired,
+  login: PropTypes.func.isRequired
+};
+
+export default connect(null, { login })(LoginPage);
+
--- src/index.js
+++ src/index.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import 'semantic-ui-css/semantic.min.css'
+import 'semantic-ui-css/semantic.min.css';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from 'redux-thunk';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import rootReducer from "./rootReducer";
+import { userLoggedIn } from "./actions/auth";
 
-ReactDOM.render(
-	<BrowserRouter> 
-		<App /> 
-	</BrowserRouter>, 
-	document.getElementById('root')
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+if (localStorage.email) {
+  //const payload = decode(localStorage.bookwormJWT);
+  const user = {
+    token: localStorage.email
+  };
+  store.dispatch(userLoggedIn(user));
+}
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App/>
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+	
 serviceWorker.unregister();
